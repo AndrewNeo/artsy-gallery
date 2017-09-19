6717623b8c6bbd8fa0753e07a21c3a8dfa3a08b7
AndrewNeo <andrew@andrewneo.com>
2017-09-19 00:55:59 -0700

    Split apart magic links and add support for a slug one

--- mdextensions.py
+++ mdextensions.py
@@ -8,31 +8,43 @@ class InternalLinksExtension(Extension):
         super(InternalLinksExtension, self).__init__(*args, **kwargs)
 
     def extendMarkdown(self, md, md_globals):
-        LINK_RE = r"<(a|c):([^>]+)>"
-        md.inlinePatterns.add("ilink", InternalLinks(LINK_RE), "<escape")
+        md.inlinePatterns.add("ilalink", TopLevelLinks(r"<a:([^>]+)>", "/{}/"), "<escape")
+        md.inlinePatterns.add("ilclink", TopLevelLinks(r"<c:([^>]+)>", "/_characters/{}.html"), "<escape")
+        md.inlinePatterns.add("ilslink", SlugLinks(r"<s:(\w+):([^>]+)>", "/{0}/{1}.html"), "<escape")
 
 
-class InternalLinks(Pattern):
-    def __init__(self, pattern):
-        super(InternalLinks, self).__init__(pattern)
+class TopLevelLinks(Pattern):
+    def __init__(self, pattern, fmt):
+        super(TopLevelLinks, self).__init__(pattern)
+        self.fmt = fmt
 
     def handleMatch(self, m):
         # <a:artist|Artist>
-        linktype = m.group(2)
-        linkcat = "characters"
-        if linktype == "a":
-            linkcat = "/{}/"
-        elif linktype == "c":
-            linkcat = "/_characters/{}.html"
-        else:
-            return "`unresolved link`"
-
-        inner = m.group(3).split("|")
+        inner = m.group(2).split("|")
         linkdest = linktext = inner[0]
         if len(inner) > 1:
             linktext = inner[1]
 
         a = etree.Element("a")
         a.text = linktext
-        a.set("href", linkcat.format(linkdest))  # TODO: Pull full names from config
+        a.set("href", self.fmt.format(linkdest))  # TODO: Pull full names from config
+        return a
+
+
+class SlugLinks(Pattern):
+    def __init__(self, pattern, fmt):
+        super(SlugLinks, self).__init__(pattern)
+        self.fmt = fmt
+
+    def handleMatch(self, m):
+        # <s:artist:slug|Artist>
+        artist = m.group(2)
+        inner = m.group(3).split("|")
+        slug = linktext = inner[0]
+        if len(inner) > 1:
+            linktext = inner[1]
+
+        a = etree.Element("a")
+        a.text = linktext
+        a.set("href", self.fmt.format(artist, slug))  # TODO: Pull full title from config
         return a
